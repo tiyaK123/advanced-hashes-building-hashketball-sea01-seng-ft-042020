6ec25d232faedfa4f020989fb417406751b80f6a
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 21:40:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -48,7 +48,9 @@ hash = {:home => {:team_name =>"Brooklyn Nets",:colors => ["Black","White"],
 :blocks => 11, 
 :slam_dunks => 1}
 ] }
-:away => {:team_name => "nba",:colors => ["red","yell"],:players => [:player_name => 1,:number => 1, :shoe => 2, :points => 3, :rebounds => 3,
-:assists => 3,:steals => 3, :steals => 3, :blocks => 4, :slam_dunks => 4]}}
+:away => {:team_name => "nba",:colors => ["red","yell"],
+:players => ""}
+  
+}}
 
 end 
