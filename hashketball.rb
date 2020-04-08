c928e9275fd562729e11240d6c9e07f6284f8607
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 21:13:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
 hash = {:home => {:team_name =>"brook",:colors => ["red","blue"],
-:players => [:player_name => "Alan Anderson",:number => 1, :shoe => 2, :points => 3, :rebounds => 3,
+:players => [:player_name => "Alan Anderson",:number => 0, :shoe => 16, :points => 22, :rebounds => 3,
 :assists => 3,:steals => 3, :steals => 3, :blocks => 4, :slam_dunks => 4]}, 
 :away => {:team_name => "nba",:colors => ["red","yell"],:players => [:player_name => 1,:number => 1, :shoe => 2, :points => 3, :rebounds => 3,
 :assists => 3,:steals => 3, :steals => 3, :blocks => 4, :slam_dunks => 4]}}
