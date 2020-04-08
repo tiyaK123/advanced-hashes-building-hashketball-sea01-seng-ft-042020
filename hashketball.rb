9ea2f778b14d963f4a97b5a1edbc914c40b3162b
tiya adugna <tiyakefalee@gmail.com>
2020-04-08 20:56:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 def game_hash
 hash = {:home => {:team_name => "",:colors => [""],
 :players => [:player_name => 1,:number => 1, :shoe => 2, :points => 3, :rebounds => 3,
-:assists => 3,:steals => 3, ]}, 
+:assists => 3,:steals => 3, :steals => 3, ]}, 
 :away => {:team_name => "",:colors => [""],:players => []}}
 
 end 
